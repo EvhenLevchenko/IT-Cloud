452560f276893989e3405f1604c78ec92af37263
John <levjohn1999@gmail.com>
2017-12-10 14:35:36 +0200

    Twice commit

---
+++ src/attic/Game/Flappy/Render.java
@@ -0,0 +1,17 @@
+package attic.Game.Flappy;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Render extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        FlappyBird.flappyBird.repaint(g);
+    }
+
+}
---
+++ src/attic/List/LinkedList/DescendingIterator.java
@@ -0,0 +1,9 @@
+package attic.List.LinkedList;
+
+
+import java.util.Iterator;
+
+public interface DescendingIterator<E> {
+    Iterator<E> descendingIterator();
+
+}
