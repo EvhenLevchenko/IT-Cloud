8c9bc09959588263d5e1027b61c285c328acae11
artemsilac <artemsilac@gmail.com>
2017-12-22 17:25:06 +0200

    First commit112

--- src/moduleTwice/labwork217/labwork3/Main.java
+++ src/moduleTwice/labwork217/labwork3/Main.java
@@ -1,39 +1,36 @@
 package moduleTwice.labwork217.labwork3;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] myArray=new int[1000];
+        final int SIZE_OF_ARRAY = 1000;
+        int[] myArray = new int[SIZE_OF_ARRAY];
 
-        MySumCount sumCount = new MySumCount(10, 15);
-        MySumCount sumCount1 = new MySumCount(150, 190);
 
-        Random random =new Random(1000);
-        for (int i = 0; i <1000 ; i++) {
-            int temp=random.nextInt();
-            myArray[i]=temp;
-
-            sumCount.setArray(myArray);
-            sumCount1.setArray(myArray);
-
-            Thread thread=new Thread(sumCount);
-            Thread thread1=new Thread(sumCount1);
-            thread.start();
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            thread1.start();
-            try {
-                thread1.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("The sum of 1st array" + sumCount.getResultSum());
-            System.out.println("The sum of 2nd array" + sumCount1.getResultSum());
+        for (int i = 0; i < SIZE_OF_ARRAY; i++) {
+            myArray[i] = new Random().nextInt(SIZE_OF_ARRAY);
         }
+
+        MySumCount mySumCount = new MySumCount(1, 2);
+
+
+        mySumCount.setArray(myArray);
+
+
+        Thread thread = new Thread(mySumCount);
+
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Arrays.toString(myArray));
+        System.out.println("The sum of 1st array " + mySumCount.getResultSum());
     }
 }
+
