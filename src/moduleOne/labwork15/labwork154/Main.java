d832137431d2f0e2b10c25639672ec796e74bf2d
JohnLevchenko <33178789+JohnLevchenko@users.noreply.github.com>
2018-01-01 18:48:49 +0200

    Update Main.java

--- src/moduleOne/labwork15/labwork154/Main.java
+++ src/moduleOne/labwork15/labwork154/Main.java
@@ -3,11 +3,11 @@ package moduleOne.labwork15.labwork154;
 public class Main {
     public static void main(String[] args) {
         int a = 0;
-        for (int i = 1; i < 300; i++) {
-            if ((i % 4 == 0) || (i % 3 == 0)) {
+        for (int i = 1; i < 300 && 10; i++) {
+            if ((i % 4 == 0 || i % 3 == 0)) {
                 System.out.println(i);
-                a++;
-                if (a == 10) break;
+       //         a++;
+       //         if (a == 10) break;
             }
         }
     }
