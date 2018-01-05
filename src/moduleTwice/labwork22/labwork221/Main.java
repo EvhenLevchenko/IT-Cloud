7d21da4aed9eaf292ef155bb12ef034e81bf2f42
John <levjohn1999@gmail.com>
2018-01-05 17:39:31 +0200

    05

--- src/moduleTwice/labwork22/labwork221/Main.java
+++ src/moduleTwice/labwork22/labwork221/Main.java
@@ -31,61 +31,41 @@ public class Main {
 
 
         System.out.println("Matrix sum");
+        Matrix.matrixSum(matrixA, matrixB);
+        System.out.println("Matrix Multiplication");
+        Matrix.matrixMultiplication(matrixA, matrixB);
+
+    }
+
+}
+
+
+class Matrix {
+    static void matrixSum(int[][] matrixA, int[][] matrixB) {
+        final int ROW = 4;
+        final int COLUME = 4;
+        int[][] matrixSum = new int[ROW][COLUME];
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COLUME; j++) {
-                int[][] matrixSum = new int[ROW][COLUME];
                 matrixSum[i][j] = matrixA[i][j] + matrixB[i][j];
                 System.out.print(matrixSum[i][j] + "\t");
             }
             System.out.println();
         }
+        System.out.println();
+    }
 
-//        System.out.println("------");
-//        for (int i = 0; i < ROW; i++) {
-//            for (int j = 0; j < COLUME; j++) {
-//                System.out.printf("%3d", matrixSum[i][j]);
-//            }
-//            System.out.println();
-//        }
-        //  Matrix.matrixSum(matrixA, matrixB);
-        System.out.println("Matrix Multiplication");
+    static void matrixMultiplication(int[][] matrixA, int[][] matrixB) {
+        final int ROW = 4;
+        final int COLUME = 4;
+        int[][] matrixMultiplication = new int[ROW][COLUME];
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COLUME; j++) {
-                int[][] matrixMultiplication = new int[ROW][COLUME];
                 matrixMultiplication[i][j] = matrixA[i][j] * matrixB[i][j];
-                System.out.print(matrixMultiplication[i][j] + " \t");
+                System.out.print(matrixMultiplication[i][j] + "\t");
             }
             System.out.println();
         }
         System.out.println();
-//        Matrix.matrixMultiplication(matrixA, matrixB);
-
     }
 }
-
-//class Matrix {
-
-//    static void matrixSum(int[][] matrixA, int[][] matrixB) {
-//        int[][] matrixSum = new int[3][3];
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                matrixSum[i][j] = matrixA[i][j] + matrixB[i][j];
-//                System.out.print(matrixSum[i][j] + "\t");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println();
-//    }
-
-//    static void matrixMultiplication(int[][] matrixA, int[][] matrixB) {
-//        int[][] matrixMultiplication = new int[3][3];
-//        for (int i = 0; i < 3; i++) {
-//            for (int j = 0; j < 3; j++) {
-//                matrixMultiplication[i][j] = matrixA[i][j] * matrixB[j][i];
-//                System.out.print(matrixMultiplication[i][j] + "  \t");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println();
-//    }
-//}
