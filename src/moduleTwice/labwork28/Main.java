ce7b68391f1dc6e09f3cf9a972c92123ad9f437d
John <levjohn1999@gmail.com>
2018-01-07 22:58:38 +0200

    07.01.2018

--- src/moduleTwice/labwork28/Main.java
+++ src/moduleTwice/labwork28/Main.java
@@ -2,28 +2,10 @@ package moduleTwice.labwork28;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Shape[] array = new Shape[9];
-        array[0] = new Rectangle("Red", 1, 1);
-        array[1] = new Rectangle("Black", 1, 2);
-        array[2] = new Rectangle("Blue", 2, 3);
-        array[3] = new Rectangle("White", 3, 4);
-        array[4] = new Rectangle("Orange", 4, 5);
-        array[5] = new Circle("Brown", 6);
-        array[6] = new Circle("Pink", 7);
-        array[7] = new Triagle("Grey", 8, 9, 10);
-        array[8] = new Triagle("Coral", 11, 12, 13);
-        Arrays.sort(array, new ShapeColor());
-        for (Shape shape : array) {
-            shape.draw();
-        }
-
-        System.out.println();
 
         Rectangle rectangleFirst = new Rectangle("Red", 1, 1);
         Rectangle rectangleTwice = new Rectangle("Orange", 4, 5);
@@ -33,14 +15,31 @@ public class Main {
         System.out.println();
 
 
-        Rectangle[] array1 = new Rectangle[6];
-        array1[0] = new Rectangle("Red", 1, 1);
-        array1[1] = new Rectangle("Black", 44, 2);
-        array1[2] = new Rectangle("Blue", 42, 4);
-        array1[3] = new Rectangle("White", 14, 99);
-        array1[4] = new Rectangle("Orange", 11, 5);
-        array1[5] = new Rectangle("Pink", 4, 5);
-        for (Shape shape : array1) {
+        Rectangle[] rectangles = new Rectangle[6];
+        rectangles[0] = new Rectangle("Red", 1, 1);
+        rectangles[1] = new Rectangle("Black", 44, 2);
+        rectangles[2] = new Rectangle("Blue", 42, 4);
+        rectangles[3] = new Rectangle("White", 14, 99);
+        rectangles[4] = new Rectangle("Orange", 11, 5);
+        rectangles[5] = new Rectangle("Pink", 4, 5);
+        Arrays.sort(rectangles, new ShapeColorComparator());
+        for (Rectangle rectangle : rectangles) {
+            System.out.println(rectangle);
+        }
+
+        System.out.println();
+        Shape[] shapes = new Shape[9];
+        shapes[0] = new Rectangle("Red", 1, 1);
+        shapes[1] = new Rectangle("Black", 1, 2);
+        shapes[2] = new Rectangle("Blue", 2, 3);
+        shapes[3] = new Rectangle("White", 3, 4);
+        shapes[4] = new Rectangle("Orange", 4, 5);
+        shapes[5] = new Circle("Brown", 6);
+        shapes[6] = new Circle("Pink", 7);
+        shapes[7] = new Triagle("Grey", 8, 9, 10);
+        shapes[8] = new Triagle("Coral", 11, 12, 13);
+        Arrays.sort(shapes, new ShapeColorComparator());
+        for (Shape shape : shapes) {
             shape.draw();
         }
     }
--- src/moduleTwice/labwork28/ShapeColorComparator.java
+++ src/moduleTwice/labwork28/ShapeColorComparator.java
@@ -2,7 +2,7 @@ package moduleTwice.labwork28;
 
 import java.util.Comparator;
 
-public class ShapeColor implements Comparator {
+public class ShapeColorComparator implements Comparator {
     @Override
     public int compare(Object o1, Object o2) {
         Shape shape1 = (Shape) o1;
